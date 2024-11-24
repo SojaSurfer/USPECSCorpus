50e60b909a1eb85141ff8851951bab7c3c051ef9
Julian Wagner <julian.s.wagner@t-online.de>
2024-11-24 13:43:47 +0100

    word frequency per candidate

--- analysis/wordFrequency.py
+++ analysis/wordFrequency.py
@@ -1,8 +1,12 @@
 import string
+import sys
 
 import xml.etree.ElementTree as ET
 from matplotlib import pyplot as plt
 from nltk import FreqDist
+import pandas as pd
+import seaborn as sns
+
 
 
 # Parse the XML file
@@ -12,23 +16,44 @@ root = tree.getroot()
 namespaces = {'tei': 'http://www.tei-c.org/ns/1.0'}
 
 
-# get most frequent words
-# Find all <w> tags with the attribute pos='NN' within the namespace
-words = root.findall(".//tei:w[@pos='NN']", namespaces)
+df = pd.read_csv('data/metadata.csv')
+speakers = df['speaker'].unique()
 
 maxWords = 30
 punctuations = string.punctuation + '—'
 
-fdist = FreqDist([word.text for word in words if not word.text in punctuations])
+sns.set_style('darkgrid')
+fig, axes = plt.subplots(4, 2, figsize=(17, 11))
 
-fig, ax = plt.subplots(figsize=(12, 8))
-ax = fdist.plot(maxWords, cumulative=False, title='Most frequent NN words')
 
-ax.set_xlabel('Words')
-ax.set_ylabel('Frequency')
-ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')  # Rotate x-axis labels for better readability
-ax.set_xlim([-0.5, maxWords-0.5])
+for i, ax in enumerate(axes.flatten()):
 
+    if i >= len(speakers):
+        break
+
+    speaker = speakers[i]
+
+    # get most frequent words
+    # Find all <w> tags with the attribute pos='NN' within the namespace
+    words = root.findall(f".//tei:div[@speaker='{speaker}']//tei:w[@pos='NN']", namespaces)
+
+    fdist = FreqDist([word.text for word in words if not word.text in punctuations])
+    mostCommon = fdist.most_common(maxWords)
+    words = [m[0] for m in mostCommon]
+    count = [m[1] for m in mostCommon]
+
+
+    ax.plot(words, count)
+    # fdist.plot(maxWords, cumulative=False, title=)
+
+    #ax.set_xlabel('Words')
+    ax.set_ylabel('Frequency')
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=45, ha='right')  # Rotate x-axis labels for better readability
+    ax.set_xlim([-0.5, maxWords-0.5])
+    ax.grid(which='major', axis='y')
+    ax.set_title(speaker)
+
+plt.suptitle('Most frequent NN words')
 plt.tight_layout()
 plt.show()
 plt.close()
